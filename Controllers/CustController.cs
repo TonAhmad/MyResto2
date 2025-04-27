edb012a75d12be26676e4f6923f0e79ca24613ef
Fathon23 <nurhayusuffathon@gmail.com>
2025-04-28 00:28:50 +0700

    Initial commit

---
+++ Controllers/CustController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResto2.Controllers
+{
+    public class CustController : Controller
+    {
+        static dbResto2Entities koneksi = new dbResto2Entities();
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
